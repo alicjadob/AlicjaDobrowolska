c2ddf4336432eb7b34cb00b20c464421715ca0a4
alicjadob <latenobody@gmail.com>
2022-11-09 16:46:18 +0100

    kolkokrzyzyk

--- AlicjaDobrowolska/kolkokrzyzyk.xaml.cs
+++ AlicjaDobrowolska/kolkokrzyzyk.xaml.cs
@@ -19,6 +19,8 @@ namespace AlicjaDobrowolska
     /// </summary>
     public partial class kolkokrzyzyk : Window
     {
+        public GameLogic _GameLogic = new GameLogic();
+      
         public kolkokrzyzyk()
         {
             InitializeComponent();
@@ -28,13 +30,38 @@ namespace AlicjaDobrowolska
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var space = (Button)sender;
+
             if (!String.IsNullOrWhiteSpace(space.Content?.ToString())) return;
+            space.Content = _GameLogic.CurrentPlayer;
+
+            var coordinates = space.Tag.ToString().Split(',');
+            var xValue = int.Parse(coordinates[0]);
+            var yValue = int.Parse(coordinates[1]);
+
+            var buttonPosition = new Position() { x = xValue, y = yValue };
+            _GameLogic.UpdateBoard(buttonPosition, _GameLogic.CurrentPlayer);
+
+            if(_GameLogic.PlayerWin())
+            {
+                WinScreen.Text = $"{_GameLogic.CurrentPlayer} WINS";
+                WinScreen.Visibility = Visibility.Visible;
+            }
+
+            _GameLogic.SetNextPlayer();
         }
 
-
-        private void Button_Restart(object sender, RoutedEventArgs e)
+        private void btnNewGame_Click(object sender, RoutedEventArgs e)
         {
-
+            foreach( var control in ticTacGrid.Children)
+            {
+                if(control is Button)
+                {
+                    ((Button)control).Content = String.Empty;
+                }
+            }
+            _GameLogic = new GameLogic();
+            WinScreen.Visibility = Visibility.Collapsed;
         }
+
     }
 }
