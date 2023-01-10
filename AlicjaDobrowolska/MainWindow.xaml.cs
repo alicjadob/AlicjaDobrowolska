bd5980e8cb1076f3601dc31ee46984c6a5abf230
alicjadob <latenobody@gmail.com>
2023-01-10 22:30:03 +0100

    movies

--- AlicjaDobrowolska/MainWindow.xaml.cs
+++ AlicjaDobrowolska/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace AlicjaDobrowolska
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Movies> listOfMovies = new List<Movies>();
         public MainWindow()
         {
             InitializeComponent();
+            listOfMovies.Add(new Movies("one", "xxxx"));
+            listOfMovies.Add(new Movies("two", "yyyy"));
+            listOfMovies.Add(new Movies("three", "zzzz"));
+            dataGridMovies.ItemsSource = listOfMovies;
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+            Window1 okno = new Window1();
+            Movies filmy = new Movies();
+            okno.DataContext = filmy;
+            okno.ShowDialog();
+            if (okno.IsEditPressed)
+            {
+                listOfMovies.Add(filmy);
+                dataGridMovies.Items.Refresh();
+            }
+        }
+
+        private void Button_Properties(object sender, RoutedEventArgs e)
+        {
+            if (dataGridMovies.SelectedItem != null)
+            {
+                Window1 okno = new Window1();
+                Movies filmy = new Movies((Movies)dataGridMovies.SelectedItem);
+                okno.DataContext = filmy;
+                okno.ShowDialog();
+                if (okno.IsEditPressed)
+                {
+                    int index = listOfMovies.IndexOf((Movies)dataGridMovies.SelectedItem);
+                    listOfMovies[index] = filmy;
+                    dataGridMovies.Items.Refresh();
+                }
+            }
         }
     }
 }
---
+++ AlicjaDobrowolska/Movies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlicjaDobrowolska
+{
+    public class Movies
+    {
+        public string title { get; set; }
+        public string type { get; set; }
+        public Movies()
+        {
+
+        }
+        public Movies(string title, string type)
+        {
+            this.title = title;
+            this.type = type;
+        }
+        public Movies(Movies filmy)
+        {
+            this.title = filmy.title;
+            this.type = filmy.type;
+        }
+    }
+}
