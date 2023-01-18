958040a462e8220e4e9d28a4f3a5298aa9d3d36f
alicjadob <latenobody@gmail.com>
2023-01-18 14:46:06 +0100

    dwa

--- AlicjaDobrowolska/MainWindow.xaml.cs
+++ AlicjaDobrowolska/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,55 @@ namespace AlicjaDobrowolska
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Kino> listOfMovies = new List<Kino>();
         public MainWindow()
         {
             InitializeComponent();
+            if (File.Exists("C:\\Users\\laten\\OneDrive\\Pulpit\\test.xml"))
+            {
+                listOfMovies = Serializacja.DeserializeToObject<List<Kino>>("C:\\Users\\laten\\OneDrive\\Pulpit\\test.xml");
+            }
+            else
+            {
+                listOfMovies.Add(new Kino("Title", "Type"));
+                listOfMovies.Add(new Kino("Title", "Type"));
+                listOfMovies.Add(new Kino("Title", "Type"));
+                listOfMovies.Add(new Kino("Title", "Type"));
+            }
+            dataGridMovies.ItemsSource = listOfMovies;
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Serializacja.SerializeToXml<List<Kino>>(listOfMovies, "C:\\Users\\laten\\OneDrive\\Pulpit\\test.xml");
+        }
+        private void Button_Zamow(object sender, RoutedEventArgs e)
+        {
+            Window1 okno = new Window1();
+            Kino filmy = new Kino();
+            okno.DataContext = filmy;
+            okno.ShowDialog();
+            if (okno.IsEditPressed)
+            {
+                listOfMovies.Add(filmy);
+                dataGridMovies.Items.Refresh();
+            }
+        }
+
+        private void Button_Wlasciwosci(object sender, RoutedEventArgs e)
+        {
+            if (dataGridMovies.SelectedItem != null)
+            {
+                Window1 okno = new Window1();
+                Kino filmy = new Kino((Movies)dataGridMovies.SelectedItem);
+                okno.DataContext = filmy;
+                okno.ShowDialog();
+                if (okno.IsEditPressed)
+                {
+                    int index = listOfMovies.IndexOf((Movies)dataGridMovies.SelectedItem);
+                    listOfMovies[index] = filmy;
+                    dataGridMovies.Items.Refresh();
+                }
+            }
         }
     }
 }
