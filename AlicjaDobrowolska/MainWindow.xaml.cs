2cc90843956ec0b7d63fc78cb140020dcdda1411
alicjadob <latenobody@gmail.com>
2023-02-10 14:38:00 +0100

    zmiana

--- AlicjaDobrowolska/MainWindow.xaml.cs
+++ AlicjaDobrowolska/MainWindow.xaml.cs
@@ -8,82 +8,9 @@ namespace AlicjaDobrowolska
     /// </summary>
     public partial class kalkulator : Window
     {
-        double first;
-        double second;
-        char op;
         public kalkulator()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Button btn = (Button)sender;
-            textBox1.Text += btn.Content.ToString();
-            second = float.Parse(textBox1.Text);
-        }
-
-        private void Button_C(object sender, RoutedEventArgs e)
-        {
-            textBox1.Clear();
-        }
-
-        private void Button_plus(object sender, RoutedEventArgs e)
-        {
-            first = float.Parse(textBox1.Text);
-            op = '+';
-            textBox1.Clear();
-        }
-
-        private void Button_minus(object sender, RoutedEventArgs e)
-        {
-            first = float.Parse(textBox1.Text);
-            op = '-';
-            textBox1.Clear();
-        }
-
-        private void Button_div(object sender, RoutedEventArgs e)
-        {
-            first = float.Parse(textBox1.Text);
-            op = '/';
-            textBox1.Clear();
-        }
-        private void Button_mult(object sender, RoutedEventArgs e)
-        {
-            first = float.Parse(textBox1.Text);
-            op = '*';
-            textBox1.Clear();
-        }
-        private void Button_equals(object sender, RoutedEventArgs e)
-        {
-            second = float.Parse(textBox1.Text);
-            double result = 0;
-            if(op == '+')
-            {
-                result = first + second;
-                first = result;
-            }
-            else if (op == '-')
-            {
-                result = first - second;
-                first = result;
-            }
-            else if (op == '*')
-            {
-                result = first * second;
-                first = result;
-            }
-            else if (op == '/')
-            {
-                result = first / second;
-                first = result;
-            }
-            if(textBox1.Text=="0")
-            {
-                textBox1.Clear();
-            }
-
-            textBox1.Text = result.ToString();
         }
     }
 }
